9cac75883d30e86b83cce257be6261c03321f3a1
Mokány Ádám <mokanyadam@gmail.com>
2022-11-25 17:01:05 +0100

    - Defining pages

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+import NewMeetupFroms from '../../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+
+const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData)
+}
+ 
+    return ( <NewMeetupFroms onAddMeetup={addMeetupHandler}></NewMeetupFroms> );
+}
+
+export default NewMeetupPage;
